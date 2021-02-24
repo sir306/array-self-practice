17bc7dc498cbe4e6c4c3a6f187400aa9f1aa7e06
nicholas harding <hardingnick00@gmail.com>
2021-02-25 12:00:13 +1300

    multi array

--- array self practice/Program.cs
+++ array self practice/Program.cs
@@ -24,6 +24,22 @@ namespace ArraySelfPractice
                 Console.WriteLine("this is : {0}", item);
             }
             Console.WriteLine("Hello World!");
+
+            string[,] multiString = new string[3, 3];
+            multiString[0, 0] = "this";
+            multiString[0, 1] = "is";
+            multiString[0, 2] = "a";
+            multiString[1, 0] = "multi";
+            multiString[1, 1] = "array";
+            multiString[1, 2] = "that";
+            multiString[2, 0] = "i";
+            multiString[2, 1] = "made";
+            multiString[2, 2] = "!";
+            
+            foreach(string item in multiString)
+            {
+                Console.Write("{0} ", item);
+            }
         }
     }
 }
