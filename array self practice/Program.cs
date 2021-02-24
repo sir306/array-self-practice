0989d1253b50cb37716672d61af9550889838757
nicholas harding <hardingnick00@gmail.com>
2021-02-25 10:15:53 +1300

    Add project files.

---
+++ array self practice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace ArraySelfPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 1, 2, 3, 4, 5, 6 };
+            string[] array2 = { "hello", "bye" };
+
+            foreach (int item in array1)
+            {
+                Console.WriteLine("this is : {0}", item);
+            }
+            foreach (string item in array2)
+            {
+                Console.WriteLine("this is : {0}", item);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
